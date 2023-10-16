c8d27f929034ae2c894e7d7f90d7208c4c77c7a6
RAJAONARISON Tsirinirina <tsirinirina.rajaonarison@dreamslab.fr>
2023-10-16 15:51:18 +0300

    Changement

--- book-service/src/app.service.ts
+++ book-service/src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import configuration from './config.constant';
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    const mongoConf = configuration().mongoConf;
-    console.log(mongoConf);
-
     return 'Hello World!';
   }
 }
